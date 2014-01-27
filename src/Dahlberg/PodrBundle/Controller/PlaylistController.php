52a358ad4ead83161c118cf6abe7c0766401b01e
Johan Dahlberg <johan@dahlberg.co>
2014-01-27 13:14:43 +0100

    + Working playlist index

--- src/Dahlberg/PodrBundle/Controller/PlaylistController.php
+++ src/Dahlberg/PodrBundle/Controller/PlaylistController.php
@@ -5,8 +5,27 @@ namespace Dahlberg\PodrBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PlaylistController extends Controller {
+    public function detailsAction($id) {
+        $user = $this->get('security.context')->getToken()->getUser();
+        $em = $this->getDoctrine()->getManager();
+
+        $playlist = $em->getRepository('DahlbergPodrBundle:Playlist')->findOneBy(array('id' => $id, 'owner' => $user));
+
+        return $this->render('DahlbergPodrBundle:Playlist:details.html.twig', array(
+            'playlist'  => $playlist,
+        ));
+    }
+
     public function indexAction() {
+        $user = $this->get('security.context')->getToken()->getUser();
+        $em = $this->getDoctrine()->getManager();
+
+        $playlists = $em->getRepository('DahlbergPodrBundle:Playlist')->findBy(
+                array('owner' => $user),
+                array('title' => 'ASC'));
+
         return $this->render('DahlbergPodrBundle:Playlist:index.html.twig', array(
+            'playlists' => $playlists
         ));
     }
 }
